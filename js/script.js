fb5fdab22cb514f18feb214e2b36189ce8c5bddf
wdharmawan <wisnudharmawan844@gmail.com>
2024-03-26 23:16:34 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusSlide(n) {
+//     showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//     var i;
+//     var slideList = document.getElementsByClassName("mySlide");
+//     if (n > slideList.length) slideIndex = 1
+//     else if ( n < 1) slideIndex =  slideList.length;
+
+//     for (i = 0; i < slideList.length; i++) {
+//         slideList[i].style.display ="none";
+//     }
+
+//     slideList[slideIndex-1].style.display ="block";
+
+// }
